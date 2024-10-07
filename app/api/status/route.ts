af33e7907ca6ede330fefdc3ea0ec9a6e3328ae8
Mykhailo Androsuyk <m.androsuik@ideil.com>
2024-10-07 12:48:40 +0300

    update status api

--- app/api/status/route.ts
+++ app/api/status/route.ts
@@ -1,7 +1,7 @@
 export async function POST(request: Request) {
     const status = await request.json();
 
-    window.console.log(status);
+    console.log(status);
 
     return new Response(JSON.stringify(status));
 }
